6529c746f37b6e146e2658350183686ef5475ac9
Gradyn Wursten <me@gradyn.com>
2024-02-19 13:30:12 -0700

    work

--- src/components/NavbarElement.tsx
+++ src/components/NavbarElement.tsx
@@ -1,10 +1,13 @@
+import { CSSProperties } from "react";
+
 export interface NavbarElementProps { // exported so we can restrict navbar section children to this
     name: string
+    style?: CSSProperties | undefined
 }
 
 const NavbarElement: React.FC<NavbarElementProps> = (props: NavbarElementProps) => {
     return(
-        <div className="navbar-element">
+        <div className="navbar-item" style={props.style}>
             <p>{props.name}</p>
         </div>
     )
--- src/components/NavbarSection.tsx
+++ src/components/NavbarSection.tsx
@@ -1,5 +1,5 @@
-import { ReactElement, ReactNode } from "react"
-import { NavbarElementProps } from "./NavbarElement"
+import { ReactElement, ReactNode, useState } from "react"
+import NavbarElement, { NavbarElementProps } from "./NavbarElement"
 
 interface NavbarSection {
     name: string
@@ -7,9 +7,15 @@ interface NavbarSection {
 }
 
 const NavbarSection: React.FC<NavbarSection> = (props: NavbarSection) => {
+
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
     return (
-        <div className="navbar-section">
-            {props.children}
+        <div className="navbar-section" onMouseOver={()=>{setIsExpanded(true)}} onMouseOut={()=>{setIsExpanded(false)}}>
+            <NavbarElement name={props.name} style={{backgroundColor: "Unset"}}/>
+            <div className="navbar-container">
+                {props.children}
+            </div>
         </div>
     )
 }
