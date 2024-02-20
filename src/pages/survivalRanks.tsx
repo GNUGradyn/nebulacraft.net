5385f25617206bcdbd86654b05054b4c819315c9
Gradyn Wursten <me@gradyn.com>
2024-02-20 10:42:43 -0700

    asdf

--- src/pages/survivalRanks.tsx
+++ src/pages/survivalRanks.tsx
@@ -4,7 +4,10 @@ const survivalRanks: React.FC = () => {
     return (
         <>
             <h1 style={{textAlign: "center", margin: 30}}>Complete 3 mission ranks to unlock the rank</h1>
-            <Rank name="Wanderer"/>
+            <Rank name="Wanderer"
+                perks={["100 bonus claim blocks", "200 claim blocks per hour", "max 8000 claim blocks", "max 2 homes"]}
+                missions={[]}
+            />
         </>
     )
 }
