fb0aa0a40c7f339ddc0273424ac8bb4fcfef4ceb
Gradyn Wursten <me@gradyn.com>
2024-02-19 20:03:29 -0700

    sdf

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import "../triangle.css"
 
 interface RankProps {
@@ -7,16 +7,45 @@ interface RankProps {
 
 const Rank: React.FC<RankProps> = (props: RankProps) => {
 
-    const [expanded, setExpanded] = useState(false);
+    interface RefContent {
+        current: HTMLDivElement | null;
+        height: number;
+    }
+
+    const [expanded, setExpanded] = useState(true);
+    const contentRef: RefObject<RefContent> = useRef({ current: null, height: 0 });
+
+    const calculateContentHeight = () => {
+        if (contentRef.current && contentRef.current.current) {
+            contentRef.current.height = contentRef.current.current.scrollHeight;
+        }
+        setExpanded(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener('load', calculateContentHeight);
+        return () => {
+            window.removeEventListener('load', calculateContentHeight);
+        };
+    }, []);
 
     return (
-        <div className="rank" onClick={()=>{setExpanded(!expanded)}}>
-            <div className="rank-head">
+        <div className="rank" ref={r => {
+            if (contentRef.current) contentRef.current.current = r;
+        }}>
+            <div className="rank-head" onClick={() => { setExpanded(!expanded) }}>
                 <h1>{props.name}</h1>
-                <p><i className={expanded?"arrow up":"arrow down"}></i></p>
+                <p><i className={expanded ? "arrow up" : "arrow down"}></i></p>
             </div>
-            <div className="rank-body">
-                {}
+            <div className="rank-body" style={{ height: expanded ? (contentRef.current?.height == 0 ? "unset" : contentRef.current?.height) : 0 }}>
+                {expanded && <>
+                    <div>
+                        <h1>Perks</h1>
+                    </div>
+                    <div>
+                        <h1>Requirements</h1>
+                    </div>
+                </>}
             </div>
         </div>
     )
